6843d282fe9d55bc19ebb94a56de313e0c9f841d
DiegoALugo <diego.lugo3@upr.edu>
2025-03-08 10:48:54 -0400

    created app component

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect default path to login
+    { path: 'login', component: LoginComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
