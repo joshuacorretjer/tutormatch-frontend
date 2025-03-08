ae14e52b5aaa0d42cbdb41f38dc89414aaf3f874
DiegoALugo <diego.lugo3@upr.edu>
2025-03-08 11:13:21 -0400

    Added register, student register and tutor register components

---
+++ angular/src/app/studentreg/studentreg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studentreg',
+  imports: [],
+  templateUrl: './studentreg.component.html',
+  styleUrl: './studentreg.component.css'
+})
+export class StudentregComponent {
+
+}
---
+++ angular/src/app/tutorreg/tutorreg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorreg',
+  imports: [],
+  templateUrl: './tutorreg.component.html',
+  styleUrl: './tutorreg.component.css'
+})
+export class TutorregComponent {
+
+}
