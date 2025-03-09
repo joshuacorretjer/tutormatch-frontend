8eab2cbcca3eef47b63177aa361a481f4ad7e8bb
DiegoALugo <diego.lugo3@upr.edu>
2025-03-08 20:18:30 -0400

    Fixed the "Register Here" link in the login page

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirect default path to login
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 ];
 
-@NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
