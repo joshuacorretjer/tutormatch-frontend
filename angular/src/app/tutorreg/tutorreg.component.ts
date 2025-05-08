f984c09620fe813f9dcfc065cde60b3680a9c1d3
joshuacorretjer <60659988+joshuacorretjer@users.noreply.github.com>
2025-05-08 18:49:56 -0400

    Fixed tutorreg

--- angular/src/app/tutorreg/tutorreg.component.ts
+++ angular/src/app/tutorreg/tutorreg.component.ts
@@ -4,7 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-tutorreg',
@@ -28,7 +27,7 @@ export class TutorregComponent {
 
   //availableClasses: any[] = [];
 
-  constructor(private http: HttpClient, private router: Router, private authService: AuthService) {}
+  constructor(private http: HttpClient, private router: Router) {}
 /*
   ngOnInit() {
     this.http.get<any[]>('http://localhost:5000/api/classes').subscribe({
@@ -42,33 +41,33 @@ export class TutorregComponent {
     });
   }
 */
-onSubmit() {
-  if (this.password !== this.confirmPassword) {
-    this.errorMessage = 'Passwords do not match';
-    return;
-  }
-
-  const tutorData = {
-    username: this.username,
-    firstName: this.firstName,
-    lastName: this.lastName,
-    email: this.email,
-    password: this.password,
-    hourlyRate: this.hourlyRate,
-    extraInfo: this.extraInfo,
-    role: 'tutor'
-  };
-
-  this.authService.registerTutor(tutorData).subscribe({
-    next: () => {
-      alert('Tutor registered successfully!');
-      this.router.navigate(['/login']);
-    },
-    error: (err: any) => {
-      console.error(err);
-      this.errorMessage = 'Registration failed.';
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      this.errorMessage = 'Passwords do not match';
+      return;
     }
-  });
-}
 
+    const payload = {
+      username: this.username,
+      first_name: this.firstName,
+      last_name: this.lastName,
+      email: this.email,
+      password: this.password,
+      account_type: 'tutor',
+      hourly_rate: this.hourlyRate,
+      //classes: this.coursesTutored,
+      bio: this.extraInfo
+    };
+
+    this.http.post('http://localhost:5000/api/register', payload).subscribe({
+      next: () => {
+        alert('Registration successful!');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error(err);
+        this.errorMessage = 'Registration failed';
+      }
+    });
+  }
 }
